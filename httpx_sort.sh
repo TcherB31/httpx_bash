2432a5cedec0ef6aad9205fdeabfa8e54d087bcb
TcherBer <108927927+TcherB31@users.noreply.github.com>
2023-01-13 15:33:54 +0000

    Update httpx_sort.sh

--- httpx_sort.sh
+++ httpx_sort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-response_codes=($(httpx -silent -status-code -threads 10 -timeout 10s -o output.txt domain_list.txt))
+response_codes=($(httpx -silent -status-code -threads 10 -timeout 10 -o output.txt domain_list.txt))
 declare -A domain_codes
 for ((i=0; i<${#response_codes[@]}; i++)); do
   domain_codes[${domain_list[i]}]=${response_codes[i]}
