338825972da5ff19aa2951b8a35592fd8f8923f9
TcherBer <108927927+TcherB31@users.noreply.github.com>
2023-01-13 15:22:20 +0000

    Add files via upload

---
+++ httpx_sort.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+response_codes=($(httpx -silent -status-code -threads 10 -timeout 10s -o output.txt domain_list.txt))
+declare -A domain_codes
+for ((i=0; i<${#response_codes[@]}; i++)); do
+  domain_codes[${domain_list[i]}]=${response_codes[i]}
+done
+for domain in $(echo ${!domain_codes[@]} | tr " " "\n" | sort -n); do
+  echo "$domain: ${domain_codes[$domain]}"
+done
